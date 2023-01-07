7c1d724abcf0488d4a954b62a85ca15cd3c4f4a8
Fatma Ashraf <fatma_ashraf12@yahoo.com>
2023-01-08 01:09:02 +0200

    Create API Endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Resources\EmployeeResource;
+use App\Models\Employee;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/employees', function () {
+    $employees = Employee::orderBy('last_name', 'DESC')->get();
+
+    return EmployeeResource::collection($employees);
 });
